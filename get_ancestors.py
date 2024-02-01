dd5a80ef5ef28606a7530cf8d2f5ddc521925b80
Ryan Waples <ryan.waples@gmail.com>
2024-02-01 10:49:53 +0100

    Update get_ancestors.py

--- get_ancestors.py
+++ get_ancestors.py
@@ -11,9 +11,9 @@ max_gen_updown = int(snakemake.params.max_gen_updown)
 n = int(snakemake.wildcards.n)
 nsplit = int(snakemake.params.nsplit)
 
-# read in the pedigree
-# pedigree should include lines for each ind listed as parent
-# missing parents have id of -1 
+# read in the pedigree as a data table and put it in the right format
+# pedigree should include lines for each ind listed as parent of other inds
+# if parent is missing put value -1 in the parent1 / parent2 column 
 # we need three columns, id of the target ind, and the ids of the two parents
 pedigree = pd.read_hdf(pedigree_path)
 pedigree = pedigree[['pid', 'parent1', 'parent2']].copy()
@@ -27,7 +27,7 @@ idx_of_id['0'] = -1
 idx_of_id[0] = -1
 idx_of_id[-1] = -1
 
-# graph nodes with be indexed / label from 0 and counting up
+# nodes in the graph will be indexed / labeled from 0 and counting up by 1 
 # we assign each ind an index, and assign them to node with that index
 parent1_of_idx = dict(zip(pedigree.index.values,
                        [idx_of_id[x] for x in pedigree['parent1']]))
@@ -38,11 +38,11 @@ pedigree['parent1_idx'] =  [idx_of_id[x] for x in pedigree['parent1']]
 pedigree['parent2_idx'] =  [idx_of_id[x] for x in pedigree['parent2']]
 
 
-# edges from child -> parent
+# setup directed edges from child -> parent
 z = [(x.Index, x.parent1_idx) for x in pedigree.itertuples() if x.parent1_idx >= 0]
 y = [(x.Index, x.parent2_idx) for x in pedigree.itertuples() if x.parent2_idx >= 0]
 
-# make the directed graph with edges going from the child to the parent
+# make the actual directed graph with edges going from the child to the parent
 # up to two edges for each row in the pedigree
 graph = gt.Graph(y+z, directed=True)
 
@@ -52,7 +52,7 @@ splits = np.array_split(np.arange(graph.num_vertices()), nsplit)
 anc = list()
 # for each node (ind)
 for i in splits[n]:
-	# distance to all other nodes
+    # distance to all other nodes
     dist_map = graph.new_vp("int16_t", val=np.iinfo(np.int16).max)
     dist = gt.shortest_distance(
         graph,
@@ -63,7 +63,7 @@ for i in splits[n]:
     )
     arr = dist.a
 	
-	# record distances above 0 and below the threshold (e.g. 12)
+    # record distances above 0 and below the threshold (e.g. 12)
     ancestors = np.where(
         np.logical_and(
             arr < max_gen_updown, arr > 0
@@ -73,7 +73,8 @@ for i in splits[n]:
     for a in ancestors:
         anc.append((i, a, arr[a]))
 
-# output is one row per child-ancestor relationship, also record the distance in generations
+# output data frame with one row per child-ancestor relationship, 
+# also include the distance in generations
 anc = pd.DataFrame(anc)
 if len(anc)>0:
     anc.columns = ['child', 'ancestor', 'ngen']
